456a0cb17825f7d60aa5e6ea7d6d881dd76d6662
Antor4q <tariquelislam2015@gmail.com>
2024-04-15 22:10:56 +0600

    designed the updated profile component

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -1,12 +1,46 @@
+import { useContext, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../routes/FirebaseContext";
 
 const UpdateProfile = () => {
+
+    const {user,setUser} = useContext(AuthContext)
+    const {name,setName} = useState("")
+    const {photoURL,setPhotoURL} = useState("")
+
+    const {register,handleSubmit } = useForm()
+
+    const onSubmit = (data) => {
+           setName(data.name || "oops")
+           setPhotoURL(data.image || "oop")
+    }
+    console.log(name, photoURL)
     return (
-        <div className="lg:h-[calc(100vh-426px)]">
+        <div className="lg:max-w-[1440px] mx-auto lg:h-[calc(100vh-426px)]">
             <Helmet>
                 <title>Opulent Estates | Update Profile</title>
             </Helmet>
-            <h2 className="text-3xl text-center">You can update your profile</h2>
+          <div className="card shrink-0 w-full  lg:w-2/4 shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+            <div className="form-control">
+                    
+                    <input {...register("name", { required: true })} type="text" placeholder="Your name" className="input input-bordered"  />
+                
+                    </div>
+                    <div className="form-control">
+                   
+                    <input {...register("image", { required: true })} type="text" placeholder="Photo URL" className="input input-bordered" />
+                
+                    </div>
+                    <div className="form-control">
+                   
+                    <input {...register("email", { required: true })} type="email" placeholder="email" className="input input-bordered"  />
+                
+                    </div>
+                   <input type="submit" value="Update Profile" />
+            </form>
+          </div>
         </div>
     );
 };
