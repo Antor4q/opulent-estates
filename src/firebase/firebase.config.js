ddf3e409c31233753adba65a3480a8de3a4889fd
Antor4q <tariquelislam2015@gmail.com>
2024-04-16 20:53:28 +0600

    added env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhdZe9SrudRBi4fhO0_DgEYCXJlNHitkU",
-  authDomain: "opulent-estates-1b663.firebaseapp.com",
-  projectId: "opulent-estates-1b663",
-  storageBucket: "opulent-estates-1b663.appspot.com",
-  messagingSenderId: "170392642528",
-  appId: "1:170392642528:web:4548aedf9d1d9366800185"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
